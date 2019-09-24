f4eedace6910462dad0f1c61f0427d6982d23938
marilia <mariliamarquesm@hotmail.com>
2019-09-23 23:16:15 -0300

    #1: manutencao de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,30 @@
 class PostsController < ApplicationController
+       
     def show
         @post = Post.find(params[:id])
     end
-    
+        
     def new
-      @post = Post.new
+        @post = Post.new
     end
-  
+    
     def create
-      @post = Post.new(post_params)
-      @post.save
-      redirect_to @post
+        @post = Post.new(post_params)
+
+        @post.save
+        redirect_to @post
     end
-  
-    private def post_params
-      params.require(:post).permit(:title, :body)
+    
+    def index 
+        @posts = Post.all
     end
-  end
+
+    def edit
+        @post = Post.find(params[:id ])
+    end
+
+    private 
+        def post_params
+            params.require(:post).permit(:title, :body)
+    end
+end

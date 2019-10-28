515c5c4825434b4baef86060e2b669303726cd23
marilia <mariliamarquesm@hotmail.com>
2019-10-28 20:33:58 -0300

    #1: Manutencao de posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, :body
+    has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_182020) do
+ActiveRecord::Schema.define(version: 2019_10_28_225659) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
